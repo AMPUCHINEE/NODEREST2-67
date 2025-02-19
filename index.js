a76a5ffa75eb027c85de526aef5a3018de62c9dd
AMPUCHINEE <s6506021620113@email.kmutnb.ac.th>
2025-02-19 08:25:32 +0700

    19/2

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ app.post('/books',(req,res) => {
     });
 });
 
-app.put('/books/:id',(req,res) => {
+app.put('/books/:id', (req, res) => {
     const book = req.body;
-    db.run('UPDATE books SET title = ?, author = ? WHERE id = ?',book.title,book,author,req.params.id,function(err) {
+    db.run('UPDATE books SET title = ?, author = ? WHERE id = ?', book.title, book.author, req.params.id, function (err) {
         if (err) {
             res.status(500).send(err);
         } else {
---
+++ src/SequlizeSQLiteCRUDBook.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize('database','username','password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: './Database/SQBoohs.sqlite'
+});
+
+const Book = sequelize.define('book', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    author: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+sequelize.sync();
+
